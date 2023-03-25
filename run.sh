8acbac62d8a551a8f2b04b5c61750e97a2e4095c
Natawon <natawon9@gmail.com>
2023-03-25 22:13:14 +0700

    made changes

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###########################
+# switch to branch you want to use
+# add all added/modified files
+git add .
+# commit changes
+git commit -am "made changes"
+# push to git remote repository
+git push orin master
+###########################
+echo Press Enter...
+read
--- test.php
+++ test.php
@@ -1,3 +1,29 @@
 <?php
-echo "hello world";
+
+//Store the output of the executed command in an array
+
+exec('ls -l', $output, $status);
+
+
+//Print all return values of the executed command as array
+
+print_r($output);
+
+echo "<br/>";
+
+//Print the output of the executed command in each line
+
+foreach($output as $value)
+
+{
+
+
+        echo $value."<br />";
+
+}
+
+//Print the return status of the executed command
+
+echo $status;
+
 ?>
